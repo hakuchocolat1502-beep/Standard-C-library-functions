a4ef55f2b4737889e47be7994f6f16375d0a759a
hakutopas <238627166+hakuchocolat1502-beep@users.noreply.github.com>
2026-05-16 02:25:22 +0900

    update ft_pow

--- git_c_fanction/math/ft_pow.c
+++ git_c_fanction/math/ft_pow.c
@@ -1,21 +1,36 @@
+#include <stdio.h>
+
 int ft_pow(double x, double y)
 {
 	int ans;
 	int count;
 
-	count = 2;
-	for (ans = x; count<=y; count++)
-		ans=ans*x;
-	/*
-	ans = x;
-	while (count<=y)
-		ans=ans*x;
-		count++;
-	*/
+	count = 0;
+	ans = 1;
+	while (count<y)
+	{
+		if (y==1)
+			ans=x;
+		if (y>=2)
+		{
+			ans=ans*x;
+			count++;
+		}
+		else
+			break;
+	}
+
 	return(ans);
 }
 
 int main()
 {
+	int x;
+	int y;
 	
+	x=5; y=1;
+	printf ("%d,%d\n",x,y);
+	printf ("%d\n",ft_pow(x,y));
+
+	return(0);
 }
